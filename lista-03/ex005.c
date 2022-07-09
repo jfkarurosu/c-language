a7dc6b9c799fd10ffd1c17ba49ae900dd264fd92
João F. Carlos <joaofelipecrodrigues@gmail.com>
2022-07-09 12:15:47 -0300

    :zap: updating ex005.c

--- lista-03/ex005.c
+++ lista-03/ex005.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 
 int main(){
+    int a1, r, n, i, total = 0;
     
-
+    scanf("%d %d %d",&a1,&r, &n);
+    
+    for(i=1; i<=n; i++){
+        total += a1+(i-1)*r;
+    }
+    
+    printf("%d\n",total);
+ 
     return 0;
 }
